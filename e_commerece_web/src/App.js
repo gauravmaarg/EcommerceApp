fcdcc29ef66783dbd1f04983ad5348809d09bc37
kumar Gaurav <gauravmaarg@gmail.com>
2021-07-29 01:39:38 +0530

    hook is applied on signin and signup component

--- e_commerece_web/src/App.js
+++ e_commerece_web/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -15,14 +15,12 @@ import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
 
-class App extends React.Component {
+const App = ({checkUserSession , currentUser}) => {
 
-  componentDidMount(){
-    const {checkUserSession} = this.props
+  useEffect(() => {
     checkUserSession();
-  }
+  },[checkUserSession]);
 
-  render() {
     return (
       <div>
         <Header />
@@ -34,7 +32,7 @@ class App extends React.Component {
             exact
             path='/signin'
             render={() =>
-              this.props.currentUser ? (
+              currentUser ? (
                 <Redirect to='/' />
               ) : (
                 <SignInAndSignUpPage />
@@ -44,7 +42,6 @@ class App extends React.Component {
         </Switch>
       </div>
     );
-  }
 }
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
--- e_commerece_web/src/pages/shop/shop.component.jsx
+++ e_commerece_web/src/pages/shop/shop.component.jsx
@@ -1,27 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CollectionOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../../pages/collection/collection.container';
-import { fetchCollectionsStart} from '../../redux/shop/shop.action';
+import { fetchCollectionsStart } from '../../redux/shop/shop.action';
 
-class ShopPage extends React.Component {
+const ShopPage = ({ fetchCollectionsStart, match }) => {
 
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+  },[fetchCollectionsStart])
 
-  render() {
-    const { match } = this.props;
-    return (
-      <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-      </div>
-    );
-  }
+  return (
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+    </div>
+  );
 }
 
 const mapDispatchToProps = dispatch => ({
